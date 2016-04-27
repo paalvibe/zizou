03a511a84bdeb0914c638adb077911b64efa6f76
Paal de Vibe <paal@menneskemaskin.no>
2016-04-27 15:24:52 +0200

    improve logging

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -4,6 +4,8 @@ class BotController < ApplicationController
   def index
     slackbot = SlackBot.new(params)
 
+    puts("params:" + params.inspect)
+
     render json: { "text" => slackbot.answer }
   end
 end
