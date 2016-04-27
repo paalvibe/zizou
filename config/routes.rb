8b83542f77c0230a196629ced42725c06aa13505
Paal de Vibe <paal@menneskemaskin.no>
2016-04-27 15:01:55 +0200

    add custom path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   post 'bot' => 'bot#index'
+  post '' => 'bot#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
