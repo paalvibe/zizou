978e8479e6a29151015ce736c9998a024719a9b9
Paal de Vibe <paal@menneskemaskin.no>
2016-04-28 15:07:22 +0200

    add quotes

---
+++ lib/football_quotes.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+module FootballQuotes
+  QUOTES = [
+            "Why always me? -Mario Balotelli",
+            "Om sine svenske landslagskolleger:
+
+Journalist: 'Hvis jeg sier Anders Svensson og Kim Källström, hva sier du da?'
+
+Zlatan: 'Zlatan.'",
+            "Det John Carew gjør med en fotball, det gjør jeg med en appelsin. -Zlatan",
+            " Mourinho er Guardiolas fullstendige motsetning. Om Mourinho lyser opp et rom, kommer Guardiola inn og drar ned alle gardinene og sitter i mørket. -Zlatan",
+            "Jag behöver inte media, det är media som behöver mig. -Zlatan",
+            "Jag spelar varken svenskt eller jugoslaviskt. Jag spelar Zlatan-style. -Zlatan",
+  ]
+end
---
+++ lib/norsk_fotball.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+module NorskFotball
+  QUOTES = [
+            "Fotball er et spill uten ball. -Drillo",
+            "Å tro på at vi kan slå Spania, er som å tro på julenissen. -Drillo",
+            "Hver gang VG ringer, får jeg vondt i magen og blir kvalm. -Drillo",
+            "Det er alltid en fordel å score på sin første målsjanse. -Drillo",
+            "Jeg er mer bekymret for markedsliberalismens frammarsj i Europa.-Drillo [på spørsmål fra sportsjournalist som i 1993 var bekymret for Rune Bratseths kne]",
+            "Det er alltid en fordel å score på sin første målsjanse. -Drillo",
+            "Ronaldo? Kæm e' det? Han har jo ikke engang fota som han kan gå på. -Nils Arne Eggen",
+            "Det er alltid en fordel å score på sin første målsjanse. -Drillo",
+            "Det jeg husker best fra landslagskarrieren var den ene gangen jeg fikk en pasning fra Jan Åge Fjørtoft. -Mini Jakobsen",
+            "En gang i forrige sesong var jeg ydmyk i omtrent fjorten dager, men ingen la merke til det. -John Carew",
+            "Norges lag burde fått fredsprisen. Knapt et skudd å se. -Arne Haukvik",
+            "En dårlig avslutning fra Pascal Simpson, som forøvrig bør ta en telefon til Grethe Roede. -Ivar Hoff",
+            "Det må være en fordel for Brann at Rosenborg ikke scorer på alle sjansene sine. -Arne Scheie",
+            "Det er ikke lurt å gå ut defensivt mot oss på Lerkendal, vet du. Det er som å sende Salman Rushdie på sykkeltur i Teheran. -Nils Arne Eggen",
+            "Strømsgodsets forsvarere fortjener ikke varmt vann i dusjen etter denne kampen her! -Ivar Hoff",
+            "Verden trenger flere ydmyke genier. Det begynner å bli for få av oss. -Egil Roger (Drillo) Olsen",
+            "Du kainn itj skræm nånn te å bli go, veit du. -Nils Arne Eggen",
+  ]
+end
