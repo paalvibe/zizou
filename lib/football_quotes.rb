8e2e6aaf34d0a026136f4721503033202e71f75e
Paal de Vibe <paal@menneskemaskin.no>
2016-04-29 14:12:12 +0200

    add quote

--- lib/football_quotes.rb
+++ lib/football_quotes.rb
@@ -11,5 +11,6 @@ Zlatan: 'Zlatan.'",
             " Mourinho er Guardiolas fullstendige motsetning. Om Mourinho lyser opp et rom, kommer Guardiola inn og drar ned alle gardinene og sitter i mørket. -Zlatan",
             "Jag behöver inte media, det är media som behöver mig. -Zlatan",
             "Jag spelar varken svenskt eller jugoslaviskt. Jag spelar Zlatan-style. -Zlatan",
+            "The ball has given me everything. Just like little girls love dolls, the best toy I’ve ever had, or could ever have, is a football. The person who invented it is a true hero: nobody can top that. -Juan Roman Riquelme",
   ]
 end
