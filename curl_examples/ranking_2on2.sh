14b9ba7b014acb0232ff13a94046a61f8c11450c
Paal de Vibe <paal@menneskemaskin.no>
2016-05-12 10:30:14 +0200

    debug last_games, add curl examples

---
+++ curl_examples/ranking_2on2.sh
@@ -0,0 +1,9 @@
+slack_webhook_token=$SCH_ZIZOU_SLACK_WEBHOOK_TOKEN
+response=$(curl -X POST https://sch-zizou.herokuapp.com/bot \
+    -H "Accept: application/json" \
+    -H "Content-Type: multipart/form-data" \
+    -F "trigger_word=fifa" \
+    -F "text=fifa ranking_2on2" \
+    -F "token=$slack_webhook_token"
+)
+echo "response: $response"
