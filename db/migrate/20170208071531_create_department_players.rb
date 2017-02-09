01070ffd19bb76cd392b835707ac5345512e5452
Paal de Vibe <paal@menneskemaskin.no>
2017-02-09 14:54:53 +0100

    fix migrations

--- db/migrate/20170208071531_create_department_players.rb
+++ db/migrate/20170208071531_create_department_players.rb
@@ -1,8 +1,8 @@
 class CreateDepartmentPlayers < ActiveRecord::Migration
   def change
     create_table :department_players do |t|
-      t.references :team, index: true, foreign_key: true
-      t.string :username, index: true
+      t.references :department, index: true, foreign_key: true
+      t.string :username, index: true, unique: true
 
       t.timestamps null: false
     end
